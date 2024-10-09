b6fc397916b6a4d9712ebdc908ea288650fad9b1
gustavocravo <gustavocravo.profissional@gmail.com>
2024-10-09 19:51:59 -0300

    editando classes

--- src/main/java/com/flyeasy/controllers/AeronaveController.java
+++ src/main/java/com/flyeasy/controllers/AeronaveController.java
@@ -1,17 +1,13 @@
 package com.flyeasy.controllers;
 
-public class AeronaveController {
-    public Aeronave(String codigoAeronave, int capacidadePassageiros, double capacidadeCarga, int numeroFileiras) {
-        this.codigoAeronave = codigoAeronave;
-        this.capacidadePassageiros = capacidadePassageiros;
-        this.capacidadeCarga = capacidadeCarga;
-        this.numeroFileiras = numeroFileiras;
-    }
+import com.flyeasy.models.Aeronave;
 
-    public void exibirDisposicaoAssentos() {
-        System.out.println("Disposição de assentos para a aeronave " + codigoAeronave + ":");
-        for (int i = 1; i <= numeroFileiras; i++) {
-            for (char assento = 'A'; assento < 'A' + assentosPorFileira; assento++) {
+public class AeronaveController {
+
+    public void exibirDisposicaoAssentos(Aeronave aeronave) {
+        System.out.println("Disposição de assentos para a aeronave " + aeronave.getCodigoAeronave() + ":");
+        for (int i = 1; i <= aeronave.getNumeroFileiras(); i++) {
+            for (char assento = 'A'; assento < 'A' + aeronave.getAssentosPorFileira(); assento++) {
                 System.out.print(i + "" + assento + " ");
             }
             System.out.println();
