511de58d5a746854856c970e75368755e535f2d5
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2024-11-05 14:06:06 -0300

    Create CartaoEmbarque.java

---
+++ src/main/java/com/flyeasy/models/CartaoEmbarque.java
@@ -0,0 +1,73 @@
+package com.flyeasy.models;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class CartaoEmbarque {
+    private String nomePassageiro;
+    private String sobrenomePassageiro;
+    private String origem;
+    private String destino;
+    private Date horarioVoo;
+    private Date horarioEmbarque;
+    private String assento;
+
+    public CartaoEmbarque(String nomePassageiro, String sobrenomePassageiro, String origem,
+                          String destino, Date horarioVoo, String assento) {
+        this.nomePassageiro = nomePassageiro;
+        this.sobrenomePassageiro = sobrenomePassageiro;
+        this.origem = origem;
+        this.destino = destino;
+        this.horarioVoo = horarioVoo;
+        this.assento = assento;
+        this.horarioEmbarque = calcularHorarioEmbarque(horarioVoo);
+    }
+
+    private Date calcularHorarioEmbarque(Date horarioVoo) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(horarioVoo);
+        calendar.add(Calendar.MINUTE, -40); // Subtrai 40 minutos
+        return calendar.getTime();
+    }
+
+    // Métodos getters para os dados do cartão de embarque
+    public String getNomePassageiro() {
+        return nomePassageiro;
+    }
+
+    public String getSobrenomePassageiro() {
+        return sobrenomePassageiro;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public Date getHorarioVoo() {
+        return horarioVoo;
+    }
+
+    public Date getHorarioEmbarque() {
+        return horarioEmbarque;
+    }
+
+    public String getAssento() {
+        return assento;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        return "Cartão de Embarque:\n" +
+               "Passageiro: " + nomePassageiro + " " + sobrenomePassageiro + "\n" +
+               "Origem: " + origem + " - Destino: " + destino + "\n" +
+               "Horário do Voo: " + dateFormat.format(horarioVoo) + "\n" +
+               "Horário de Embarque: " + dateFormat.format(horarioEmbarque) + "\n" +
+               "Assento: " + assento;
+    }
+}
